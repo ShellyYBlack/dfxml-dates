cf801ad195dc1319196a24b177efbb687b0e4d89
ShellyYBlack <syblack@ncsu.edu>
2022-12-02 11:14:18 -0500

    Add mtime

--- dfxml_dates.py
+++ dfxml_dates.py
@@ -6,6 +6,7 @@ with open("dfxml.xml") as fp:
 
 fileobjects = soup.find_all('fileobject')
 with open ('dfxml-dates.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["filename", "ctime", "mtime"])
     for fileobject in fileobjects:
-        writer = csv.writer(csvfile)
-        writer.writerow([fileobject.filename.text,fileobject.ctime.text])
+        writer.writerow([fileobject.filename.text,fileobject.ctime.text,fileobject.mtime.text])
